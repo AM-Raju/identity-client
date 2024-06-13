5f866e7d6adee5bb13575ad7e2935469298b5201
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-06-14 00:07:39 +0600

    Getting data from price range done

--- src/components/products/PriceRange.tsx
+++ src/components/products/PriceRange.tsx
@@ -1,4 +1,45 @@
+"use client";
+import { ChangeEvent, useState } from "react";
+
+const priceRangeFn = (rangeArr: string[]) => {
+  let arr1 = [];
+
+  for (let range of rangeArr) {
+    arr1.push(...range.split("-"));
+  }
+
+  console.log(arr1);
+  const arr2 = arr1.sort((a, b) => Number(a) - Number(b));
+  console.log(arr2);
+
+  return {
+    startPrice: Number(arr2[0]),
+    endPrice: Number(arr2[arr2.length - 1]),
+  };
+};
+
 const PriceRange = () => {
+  const [selectedRanges, setSelectedRanges] = useState<string[]>([]);
+
+  // ["00-50", "51-100", "101-150", "151-200"];
+  let priceRange;
+  if (selectedRanges) {
+    priceRange = priceRangeFn(selectedRanges);
+  }
+
+  console.log("Price range", priceRange);
+
+  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log("Event", event);
+
+    const { name, checked } = event.target;
+
+    if (checked) {
+      setSelectedRanges([...selectedRanges, name]);
+    } else {
+      setSelectedRanges(selectedRanges.filter((range) => range !== name));
+    }
+  };
   return (
     <div className=" border border-secondary px-8 py-10">
       <h1 className="border-l-4 pl-3 border-coal-black text-xl">Price range</h1>
@@ -8,7 +49,8 @@ const PriceRange = () => {
             className=" focus:ring-0 focus:ring-offset-0  size-5"
             type="checkbox"
             name="00-50"
-            id=""
+            id="00-50"
+            onChange={handleCheckboxChange}
           />
           <label htmlFor="">$00 - $50</label>
         </div>
@@ -17,7 +59,8 @@ const PriceRange = () => {
             className=" focus:ring-0 focus:ring-offset-0  size-5"
             type="checkbox"
             name="51-100"
-            id=""
+            id="51-100"
+            onChange={handleCheckboxChange}
           />
           <label htmlFor="">$51 - $100</label>
         </div>
@@ -26,7 +69,8 @@ const PriceRange = () => {
             className=" focus:ring-0 focus:ring-offset-0  size-5"
             type="checkbox"
             name="101-150"
-            id=""
+            id="101-150"
+            onChange={handleCheckboxChange}
           />
           <label htmlFor="">$101 - $150</label>
         </div>
@@ -35,7 +79,8 @@ const PriceRange = () => {
             className=" focus:ring-0 focus:ring-offset-0  size-5"
             type="checkbox"
             name="151-200"
-            id=""
+            id="151-200"
+            onChange={handleCheckboxChange}
           />
           <label htmlFor="">$151 - $200</label>
         </div>
